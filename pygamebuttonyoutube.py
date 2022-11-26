2ec9fce5fc82b479f0ed8f64614f2333508f956a
TY-Yangg <117046256+TY-Yangg@users.noreply.github.com>
2022-11-26 13:41:32 -0500

    Update pygamebuttonyoutube.py

--- pygamebuttonyoutube.py
+++ pygamebuttonyoutube.py
@@ -1,51 +1,65 @@
 import pygame
 import sys
 
+class Button():
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        self.image = image
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.font = font
+        self.base_color, self.hovering_color = base_color, hovering_color
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
+
+    def checkForInput(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                          self.rect.bottom):
+            return True
+        return False
+
+    def changeColor(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                          self.rect.bottom):
+            self.text = self.font.render(self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(self.text_input, True, self.base_color)
+
+
+# below shows how button can be used
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Button!")
 main_font = pygame.font.SysFont("cambria", 50)
 
-class Button():
-	def __init__(self, image, x_pos, y_pos, text_input):
-		self.image = image
-		self.x_pos = x_pos
-		self.y_pos = y_pos
-		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-		self.text_input = text_input
-		self.text = main_font.render(self.text_input, True, "white")
-		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-
-	def update(self):
-		screen.blit(self.image, self.rect)
-		screen.blit(self.text, self.text_rect)
-
-	def checkForInput(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			print("Button Press!")
-
-	def changeColor(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			self.text = main_font.render(self.text_input, True, "green")
-		else:
-			self.text = main_font.render(self.text_input, True, "white")
+SCREEN = pygame.display.set_mode((1100, 1100))
 
 button_surface = pygame.image.load("button.png")
 button_surface = pygame.transform.scale(button_surface, (400, 150))
-
-button = Button(button_surface, 400, 300, "Button")
+button = Button(image=button_surface, pos=(650, 700),
+                  text_input="Quit", font=main_font, base_color="#d7fcd4", hovering_color="White")
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			button.checkForInput(pygame.mouse.get_pos())
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            button.checkForInput(pygame.mouse.get_pos())
+            pygame.quit()
+            sys.exit()
 
-	screen.fill("white")
+    screen.fill("white")
 
-	button.update()
-	button.changeColor(pygame.mouse.get_pos())
+    button.update(screen)
+    button.changeColor(pygame.mouse.get_pos())
 
-	pygame.display.update()
+    pygame.display.update()
